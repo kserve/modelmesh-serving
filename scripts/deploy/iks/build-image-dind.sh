21ae51ee39af43e76130d3174167d406e5bd394e
Chin Huang <chhuang@us.ibm.com>
2021-08-13 05:28:48 +0000

    Update docker push
    Update docker push script to use the latest commit time to
    determine whether to push a new image to Docker Hub.

--- scripts/deploy/iks/build-image-dind.sh
+++ scripts/deploy/iks/build-image-dind.sh
@@ -7,6 +7,7 @@ set -xe
 # - RUN_TASK:             execution task:
 #                           - `build`: build the image
 #                           - `build_push`: build and push the image
+#                           - `cleanup`: cleanup the image in sandbox
 #
 
 # The following envs could be loaded from `build.properties` that
@@ -55,6 +56,19 @@ build_image() {
   make build
   docker images
   docker inspect "kserve/modelmesh-controller:latest"
+
+  echo "==========================Push image to sandbox================================"
+  echo "BUILD_NUMBER=${BUILD_NUMBER}"
+  echo "ARCHIVE_DIR=${ARCHIVE_DIR}"
+  echo "GIT_BRANCH=${GIT_BRANCH}"
+  echo "GIT_COMMIT=${GIT_COMMIT}"
+  echo "GIT_COMMIT_SHORT=${GIT_COMMIT_SHORT}"
+  echo $PUBLISH_TAG
+  set +x
+  docker login -u "$DOCKERSANDBOX_USERNAME" -p "$DOCKERSANDBOX_TOKEN"
+  set -x
+  docker tag "kserve/modelmesh-controller:latest" "${DOCKERSANDBOX_NAMESPACE}/modelmesh-controller:${GIT_COMMIT_SHORT}"
+  docker push "${DOCKERSANDBOX_NAMESPACE}/modelmesh-controller:${GIT_COMMIT_SHORT}"
 }
 
 ######################################################################################
@@ -62,6 +76,21 @@ build_image() {
 ######################################################################################
 push_image() {
   echo "=======================Push image to Docker Hub==============================="
+  if [[ "$PUBLISH_TAG" == "latest" ]]; then
+    apt install jq -y
+    apt install curl -y
+    export LAST_PUSHED=$(curl -X GET https://hub.docker.com/v2/repositories/${DOCKERHUB_NAMESPACE}/modelmesh-controller/tags/latest | jq -r '.tag_last_pushed')
+    let DIFF=($(git log -1 --format=%ct)-$(date -d $LAST_PUSHED +%s))
+    if [[ "$DIFF" -gt 0 ]]; then
+      # Will proceed to push the latest image since it is not published yet
+      echo "Will push the latest image since the latest commit is more recent";
+    else
+      # The latest commit should be already published since the last pushed time is more recent
+      echo "Will not push the image since the last pushed time is more recent";
+      return 0;
+    fi
+  fi
+
   # login dockerhub
   echo $DOCKERHUB_USERNAME
   echo $DOCKERHUB_NAMESPACE
@@ -72,6 +101,19 @@ push_image() {
   docker tag "kserve/modelmesh-controller:latest" "${DOCKERHUB_NAMESPACE}/modelmesh-controller:${PUBLISH_TAG}"
   docker push "${DOCKERHUB_NAMESPACE}/modelmesh-controller:${PUBLISH_TAG}"
 }
+
+######################################################################################
+# Cleanup image                                                                      #
+######################################################################################
+cleanup_image() {
+  echo "======================Cleanup modelmesh controller image======================"
+  # Will delete the image tag from Docker Hub sandbox.
+  echo "GIT_COMMIT_SHORT=${GIT_COMMIT_SHORT}"
+  set +x
+  HUB_TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKERSANDBOX_USERNAME}'", "password": "'${DOCKERSANDBOX_TOKEN}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+  curl -H "Authorization: JWT ${HUB_TOKEN}" -X DELETE https://hub.docker.com/v2/repositories/${DOCKERSANDBOX_NAMESPACE}/modelmesh-controller/tags/${GIT_COMMIT_SHORT}/
+}
+
 case "$RUN_TASK" in
   "build")
     build_image
@@ -82,7 +124,11 @@ case "$RUN_TASK" in
     push_image
     ;;
 
+  "cleanup")
+    cleanup_image
+    ;;
+
   *)
-    echo "please specify RUN_TASK=build|build_push"
+    echo "please specify RUN_TASK=build|build_push|cleanup"
     ;;
 esac
