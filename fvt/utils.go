3a612d1095573e728e49fe2ad4a25857cf0a3171
TRAVIS JOHNSON <tsjohnso@us.ibm.com>
2021-09-01 09:02:17 -0600

    refactor(fvt): decouple test cases to support running in parallel
    * refactor(fvt): reorganize files, consolidate global vars, and create utils.go
    
    * refactor(fvt): move inference functions and expectations to inference.go
    
    * refactor(fvt): remove unnecessary schemaPath field of FVTPredictor
    
    The manifest file itself already has the schemaPath set.
    
    * refactor: use helper to have unique function names per test case
    
    * refactor: move startTime tracking and failure logging to a JustAfterEach
    
    * refactor: de-couple predictor tests
    
    - do not delete all predictors for each test
    - do not verify that all predictors are deleted
    - do not list predictors and expect one
    
    - get particular predictor to verify state
    - watch particular predictor by name per test
    - use ApplyPredictorExpectSuccess (needed with GET instead of LIST)
      - remove the setting of resourceVersion on unstructured objects
    
    - no DisconnectFromModelMesh() in AfterEach
    - DisconnectFromModelMesh() in AfterSuite instead
    - ConnectToModelMesh() does not error if port-forward already exists
    
    * refactor: move DisconnectFromModelMesh to be near related funcs
    
    * feat(fvt): set port-forward port based on ginkgo worker node index
    
    * refactor: simplify usage of WaitForStableActiveDeployState()
    
    * refactor: move helper functions to helpers.go
    
    * refactor: create and use helper funcs to create/update predictors
    
    * fmt: commit changes suggested by go fmt
    
    * fix(fvt): ensure predictors get deleted after each test
    
    * fix: disconnect from model-mesh between TLS tests
    
    This is needed now that the disconnect is not in the AfterEach.
    
    * fix: no AfterEach DeletePredictor for scale_to_zero tests that manually delete
    
    DeletePredictor fails if the Predictor doesn't exist. Also updated the sleep timers in case the deletion takes a seconds to return.
    
    * fix: ensure TLS tests are creating a new connection
    
    * fix: track port-forward failures better to fix support for connection reuse
    
    * fix: use channel to signal when the port-forward exits
    
    * fix: close grpcConn if the port-forward exits
    
    * refactor(fvt): rename ModelMesh -> ModelServing
    
    * Merge remote-tracking branch 'origin/main' into refactor-fvt
    
    * fix: split mushroom inference after merge from main

---
+++ fvt/utils.go
@@ -0,0 +1,72 @@
+// Copyright 2021 IBM Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package fvt
+
+import (
+	"io/ioutil"
+
+	. "github.com/onsi/gomega"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	yamlserializer "k8s.io/apimachinery/pkg/runtime/serializer/yaml"
+)
+
+func DecodeResourceFromFile(resourcePath string) *unstructured.Unstructured {
+	content, err := ioutil.ReadFile(resourcePath)
+	Expect(err).ToNot(HaveOccurred())
+
+	obj := &unstructured.Unstructured{}
+
+	// decode YAML into unstructured.Unstructured
+	dec := yamlserializer.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
+	decodedObj, _, err := dec.Decode([]byte(content), nil, obj)
+	Expect(err).ToNot(HaveOccurred())
+
+	obj = decodedObj.(*unstructured.Unstructured)
+	Expect(obj).ToNot(BeNil())
+	return obj
+}
+
+// Small functions to work with unstructred objects
+
+func GetInt64(obj *unstructured.Unstructured, fieldPath ...string) int64 {
+	value, _, err := unstructured.NestedInt64(obj.Object, fieldPath...)
+	Expect(err).ToNot(HaveOccurred())
+	return value
+}
+
+func GetString(obj *unstructured.Unstructured, fieldPath ...string) string {
+	value, exists, err := unstructured.NestedString(obj.Object, fieldPath...)
+	Expect(exists).To(BeTrue())
+	Expect(err).ToNot(HaveOccurred())
+	return value
+}
+
+func GetMap(obj *unstructured.Unstructured, fieldPath ...string) map[string]interface{} {
+	value, _, err := unstructured.NestedMap(obj.Object, fieldPath...)
+	Expect(err).ToNot(HaveOccurred())
+	return value
+}
+
+func SetString(obj *unstructured.Unstructured, value string, fieldPath ...string) {
+	err := unstructured.SetNestedField(obj.Object, value, fieldPath...)
+	Expect(err).ToNot(HaveOccurred())
+}
+
+func GetBool(obj *unstructured.Unstructured, fieldPath ...string) bool {
+	value, exists, err := unstructured.NestedBool(obj.Object, fieldPath...)
+	Expect(exists).To(BeTrue())
+	Expect(err).ToNot(HaveOccurred())
+	return value
+}
