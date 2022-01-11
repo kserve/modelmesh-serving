d8bbda2cc97f06eca56741e81b2b980dc89185ce
Paul Van Eck <pvaneck@us.ibm.com>
2022-01-10 16:24:12 -0800

    feat: Update InferenceService reconciliation logic (#101)
    * feat: Update InferenceService reconciliation logic
    
    The ModelMesh controller can now reconcile InferenceServices using
    the new Model Spec in the predictor. Also, an issue was fixed with there
    being a leading slash in the model path when an ISvc StorageURI was
    parsed. This was causing  issues in the adapter, preventing models from
    being loaded.
    
    Signed-off-by: Paul Van Eck <pvaneck@us.ibm.com>
    
    * FIx unit tests and make field usage more strict
    
    Signed-off-by: Paul Van Eck <pvaneck@us.ibm.com>

--- pkg/predictor_source/inferenceservice_registry.go
+++ pkg/predictor_source/inferenceservice_registry.go
@@ -21,6 +21,7 @@ import (
 	"errors"
 	"fmt"
 	"net/url"
+	"strings"
 
 	"github.com/kserve/modelmesh-serving/apis/serving/common"
 	"github.com/kserve/modelmesh-serving/apis/serving/v1alpha1"
@@ -46,25 +47,58 @@ func BuildBasePredictorFromInferenceService(isvc *v1beta1.InferenceService) (*v1
 		return nil, nil
 	}
 
+	p.ObjectMeta = isvc.ObjectMeta
+
 	framework, frameworkSpec := isvc.Spec.Predictor.GetPredictorFramework()
-	if frameworkSpec == nil {
-		return nil, errors.New("No valid InferenceService predictor framework found")
-	}
+	runtimeFromAnnotation, runtimeAnnotationExists := isvc.ObjectMeta.Annotations[v1beta1.RuntimeAnnotation]
 
-	p.Spec = v1alpha1.PredictorSpec{
-		Model: v1alpha1.Model{
-			Type: v1alpha1.ModelType{
-				Name: framework,
-			},
-		},
-	}
+	if isvc.Spec.Predictor.Model != nil {
 
-	// If explicit ServingRuntime was passed in through an annotation
-	if runtime, ok := isvc.ObjectMeta.Annotations[v1beta1.RuntimeAnnotation]; ok {
-		p.Spec.Runtime = &v1alpha1.PredictorRuntime{
-			RuntimeRef: &v1alpha1.RuntimeRef{
-				Name: runtime,
+		if frameworkSpec != nil {
+			return nil, fmt.Errorf("the InferenceService %v cannot have both the model spec and a framework spec (%v)", isvc.Name, framework)
+		}
+		if runtimeAnnotationExists {
+			return nil, fmt.Errorf("the InferenceService %v cannot have both the model spec and the "+
+				"runtime annotation %v", isvc.Name, v1beta1.RuntimeAnnotation)
+		}
+
+		p.Spec = v1alpha1.PredictorSpec{
+			Model: v1alpha1.Model{
+				Type: v1alpha1.ModelType{
+					Name:    isvc.Spec.Predictor.Model.ModelFormat.Name,
+					Version: isvc.Spec.Predictor.Model.ModelFormat.Version,
+				},
 			},
+		}
+		if isvc.Spec.Predictor.Model.Runtime != nil {
+			p.Spec.Runtime = &v1alpha1.PredictorRuntime{
+				RuntimeRef: &v1alpha1.RuntimeRef{
+					Name: *isvc.Spec.Predictor.Model.Runtime,
+				},
+			}
+		}
+	} else {
+		// Note: This block of logic is here to maintain backwards compatibility and
+		// will be removed in the future.
+		if frameworkSpec == nil {
+			return nil, errors.New("no valid InferenceService predictor framework found")
+		}
+
+		p.Spec = v1alpha1.PredictorSpec{
+			Model: v1alpha1.Model{
+				Type: v1alpha1.ModelType{
+					Name: framework,
+				},
+			},
+		}
+
+		// If explicit ServingRuntime was passed in through an annotation
+		if runtimeAnnotationExists {
+			p.Spec.Runtime = &v1alpha1.PredictorRuntime{
+				RuntimeRef: &v1alpha1.RuntimeRef{
+					Name: runtimeFromAnnotation,
+				},
+			}
 		}
 	}
 	return p, nil
@@ -73,9 +107,17 @@ func BuildBasePredictorFromInferenceService(isvc *v1beta1.InferenceService) (*v1
 // Return secretKey, bucket, modelPath, schemaPath, and error
 func processInferenceServiceStorage(inferenceService *v1beta1.InferenceService, nname types.NamespacedName) (
 	secretKey *string, parameters map[string]string, modelPath string, schemaPath *string, err error) {
-	_, frameworkSpec := inferenceService.Spec.Predictor.GetPredictorFramework()
-	storageUri := frameworkSpec.StorageURI
-	storageSpec := frameworkSpec.Storage
+
+	var pSpec *v1beta1.PredictorExtensionSpec
+	if inferenceService.Spec.Predictor.Model != nil {
+		pSpec = &inferenceService.Spec.Predictor.Model.PredictorExtensionSpec
+
+	} else {
+		_, pSpec = inferenceService.Spec.Predictor.GetPredictorFramework()
+	}
+
+	storageUri := pSpec.StorageURI
+	storageSpec := pSpec.Storage
 	uriParameters := make(map[string]string)
 
 	if storageUri == nil {
@@ -98,7 +140,7 @@ func processInferenceServiceStorage(inferenceService *v1beta1.InferenceService, 
 
 		switch u.Scheme {
 		case "s3":
-			modelPath = u.Path
+			modelPath = strings.TrimPrefix(u.Path, "/")
 			uriParameters["type"] = "s3"
 			uriParameters["bucket"] = u.Host
 		// TODO: Support StorageURI for other types of storage too
@@ -168,7 +210,6 @@ func (isvcr InferenceServiceRegistry) Get(ctx context.Context, nname types.Names
 		return nil, nil
 	}
 
-	p.ObjectMeta = inferenceService.ObjectMeta
 	p.Status = inferenceService.Status.PredictorStatus
 
 	if p.Status.ActiveModelState == "" {
--- pkg/predictor_source/inferenceservice_registry_test.go
+++ pkg/predictor_source/inferenceservice_registry_test.go
@@ -20,8 +20,178 @@ import (
 	"github.com/kserve/modelmesh-serving/apis/serving/v1beta1"
 
 	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
+
+func TestBuildBasePredictorFromInferenceService_ModelSpecSimple(t *testing.T) {
+	formatName := "tensorflow"
+	inferenceService := &v1beta1.InferenceService{
+		ObjectMeta: metav1.ObjectMeta{
+			Annotations: map[string]string{
+				v1beta1.DeploymentModeAnnotation: v1beta1.MMDeploymentModeVal,
+			},
+		},
+		Spec: v1beta1.InferenceServiceSpec{
+			Predictor: v1beta1.InferenceServicePredictorSpec{
+				Model: &v1beta1.ModelSpec{
+					ModelFormat: v1beta1.ModelFormat{
+						Name: formatName,
+					},
+				},
+			},
+		},
+	}
+	predictor, err := BuildBasePredictorFromInferenceService(inferenceService)
+	assert.NoError(t, err)
+	assert.Equal(t, formatName, predictor.Spec.Model.Type.Name)
+	assert.Nil(t, predictor.Spec.Model.Type.Version)
+	assert.Nil(t, predictor.Spec.Runtime)
+}
+
+func TestBuildBasePredictorFromInferenceService_ModelSpecRuntime(t *testing.T) {
+	formatName := "tensorflow"
+	formatVersion := "1"
+	runtimeName := "triton-x"
+	inferenceService := &v1beta1.InferenceService{
+		ObjectMeta: metav1.ObjectMeta{
+			Annotations: map[string]string{
+				v1beta1.DeploymentModeAnnotation: v1beta1.MMDeploymentModeVal,
+			},
+		},
+		Spec: v1beta1.InferenceServiceSpec{
+			Predictor: v1beta1.InferenceServicePredictorSpec{
+				Model: &v1beta1.ModelSpec{
+					ModelFormat: v1beta1.ModelFormat{
+						Name:    formatName,
+						Version: &formatVersion,
+					},
+					Runtime: &runtimeName,
+				},
+			},
+		},
+	}
+	predictor, err := BuildBasePredictorFromInferenceService(inferenceService)
+	assert.NoError(t, err)
+	assert.Equal(t, formatName, predictor.Spec.Model.Type.Name)
+	if assert.NotNil(t, predictor.Spec.Model.Type.Version) {
+		assert.Equal(t, formatVersion, *predictor.Spec.Model.Type.Version)
+	}
+	if assert.NotNil(t, predictor.Spec.Runtime) {
+		assert.Equal(t, runtimeName, predictor.Spec.Runtime.Name)
+	}
+}
+
+func TestBuildBasePredictorFromInferenceService_FrameworkSpec(t *testing.T) {
+	uri := "s3://foo/bar"
+	runtimeName := "triton-x"
+	inferenceService := &v1beta1.InferenceService{
+		ObjectMeta: metav1.ObjectMeta{
+			Annotations: map[string]string{
+				v1beta1.DeploymentModeAnnotation: v1beta1.MMDeploymentModeVal,
+				v1beta1.RuntimeAnnotation:        runtimeName,
+			},
+		},
+		Spec: v1beta1.InferenceServiceSpec{
+			Predictor: v1beta1.InferenceServicePredictorSpec{
+				SKLearn: &v1beta1.PredictorExtensionSpec{
+					StorageURI: &uri,
+				},
+			},
+		},
+	}
+	predictor, err := BuildBasePredictorFromInferenceService(inferenceService)
+	assert.NoError(t, err)
+	assert.Equal(t, "sklearn", predictor.Spec.Model.Type.Name)
+	assert.Nil(t, predictor.Spec.Model.Type.Version)
+	if assert.NotNil(t, predictor.Spec.Runtime) {
+		assert.Equal(t, runtimeName, predictor.Spec.Runtime.Name)
+	}
+}
+
+func TestBuildBasePredictorFromInferenceService_InvalidSpec(t *testing.T) {
+	inferenceService := &v1beta1.InferenceService{
+		ObjectMeta: metav1.ObjectMeta{
+			Annotations: map[string]string{
+				v1beta1.DeploymentModeAnnotation: v1beta1.MMDeploymentModeVal,
+			},
+		},
+		Spec: v1beta1.InferenceServiceSpec{},
+	}
+	predictor, err := BuildBasePredictorFromInferenceService(inferenceService)
+	assert.Error(t, err)
+	assert.Nil(t, predictor)
+}
+
+func TestBuildBasePredictorFromInferenceService_NonMM(t *testing.T) {
+	inferenceService := &v1beta1.InferenceService{
+		ObjectMeta: metav1.ObjectMeta{},
+		Spec: v1beta1.InferenceServiceSpec{
+			Predictor: v1beta1.InferenceServicePredictorSpec{
+				Model: &v1beta1.ModelSpec{
+					ModelFormat: v1beta1.ModelFormat{
+						Name: "foo",
+					},
+				},
+			},
+		},
+	}
+	predictor, err := BuildBasePredictorFromInferenceService(inferenceService)
+	assert.NoError(t, err)
+	assert.Nil(t, predictor)
+}
+
+func TestBuildBasePredictorFromInferenceService_BothSpecs(t *testing.T) {
+	uri := "s3://foo/bar"
+	inferenceService := &v1beta1.InferenceService{
+		ObjectMeta: metav1.ObjectMeta{
+			Annotations: map[string]string{
+				v1beta1.DeploymentModeAnnotation: v1beta1.MMDeploymentModeVal,
+			},
+		},
+		Spec: v1beta1.InferenceServiceSpec{
+			Predictor: v1beta1.InferenceServicePredictorSpec{
+				Model: &v1beta1.ModelSpec{
+					ModelFormat: v1beta1.ModelFormat{
+						Name: "foo",
+					},
+				},
+				SKLearn: &v1beta1.PredictorExtensionSpec{
+					StorageURI: &uri,
+				},
+			},
+		},
+	}
+	predictor, err := BuildBasePredictorFromInferenceService(inferenceService)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "cannot have both the model spec and a framework")
+	assert.Nil(t, predictor)
+}
+
+func TestBuildBasePredictorFromInferenceService_AnnotationWithModelSpec(t *testing.T) {
+	runtimeName := "mlserver-x"
+	inferenceService := &v1beta1.InferenceService{
+		ObjectMeta: metav1.ObjectMeta{
+			Annotations: map[string]string{
+				v1beta1.DeploymentModeAnnotation: v1beta1.MMDeploymentModeVal,
+				v1beta1.RuntimeAnnotation:        runtimeName,
+			},
+		},
+		Spec: v1beta1.InferenceServiceSpec{
+			Predictor: v1beta1.InferenceServicePredictorSpec{
+				Model: &v1beta1.ModelSpec{
+					ModelFormat: v1beta1.ModelFormat{
+						Name: "foo",
+					},
+				},
+			},
+		},
+	}
+	predictor, err := BuildBasePredictorFromInferenceService(inferenceService)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "cannot have both the model spec and the runtime annotation")
+	assert.Nil(t, predictor)
+}
 
 func TestProcessInferenceServiceStorage_Simple(t *testing.T) {
 	storageKey := "localMinIO"
@@ -56,8 +226,8 @@ func TestProcessInferenceServiceStorage_Simple(t *testing.T) {
 
 func TestProcessInferenceServiceStorage_S3UriProcessing(t *testing.T) {
 	uriBucket := "uri-bucket"
-	uriPath := "/some/path/in/the/uri"
-	uri := "s3://" + uriBucket + uriPath
+	uriPath := "some/path/in/the/uri"
+	uri := "s3://" + uriBucket + "/" + uriPath
 
 	nname := types.NamespacedName{Name: "tm-test-model", Namespace: "modelmesh-serving"}
 	inferenceService := &v1beta1.InferenceService{
