e529935b6e27211c50c95d2602294e6eb7a21aee
Christian Kadner <ckadner@us.ibm.com>
2023-03-06 16:38:24 -0800

    Temporarily disable OVMS and Triton runtimes
    Signed-off-by: Christian Kadner <ckadner@us.ibm.com>

--- controllers/servingruntime_controller_test.go
+++ controllers/servingruntime_controller_test.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -52,11 +52,12 @@ func waitForAndGetRuntimeDeployment(runtimeName string) *appsv1.Deployment {
 }
 
 var _ = Describe("Sample Runtime", func() {
+	// TODO: reinstate all runtimes
 	samplesToTest := []string{
 		"config/runtimes/mlserver-0.x.yaml",
-		"config/runtimes/triton-2.x.yaml",
-		"config/runtimes/ovms-1.x.yaml",
-		"config/runtimes/torchserve-0.x.yaml",
+		//"config/runtimes/triton-2.x.yaml",
+		//"config/runtimes/ovms-1.x.yaml",
+		//"config/runtimes/torchserve-0.x.yaml",
 	}
 	for _, f := range samplesToTest {
 		// capture the value in new variable for each iteration
