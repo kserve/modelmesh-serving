f47cd7b755f491e40aa91bf52ec132a473fa1316
Jan Wytze Zuidema <info@janwytze.nl>
2023-09-19 09:15:41 +0200

    fix: Support devel version of Kustomize in install script (#428)
    Instead of comparing the version of the installed kustomize CLI,
    use the kustomize man pages to determine the supported 
    format of the --load-restrictor parameter.
    
    Closes #427 
    
    Signed-off-by: Jan Wytze Zuidema <janwytze@klippa.com>
    Signed-off-by: Christian Kadner <ckadner@us.ibm.com>
    Co-authored-by: Christian Kadner <ckadner@us.ibm.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -378,13 +378,7 @@ wait_for_pods_ready "-l control-plane=modelmesh-controller"
 
 # Older versions of kustomize have different load restrictor flag formats.
 # Can be removed once Kubeflow installation stops requiring v3.2.
-kustomize_version=$(kustomize version --short | grep -o -E "[0-9]\.[0-9]\.[0-9]")
-kustomize_load_restrictor_arg="--load-restrictor LoadRestrictionsNone"
-if [[ -n "$kustomize_version" && "$kustomize_version" < "3.4.0" ]]; then
-    kustomize_load_restrictor_arg="--load_restrictor none"
-elif [[ -n "$kustomize_version" && "$kustomize_version" < "4.0.1" ]]; then
-    kustomize_load_restrictor_arg="--load_restrictor LoadRestrictionsNone"
-fi
+kustomize_load_restrictor_arg=$( kustomize build --help | grep -o -E "\-\-load.restrictor[^,]+" | sed -E "s/(--load.restrictor).+'(.*none)'/\1 \2/I" )
 
 info "Installing ModelMesh Serving built-in runtimes"
 if [[ $namespace_scope_mode == "true" ]]; then
